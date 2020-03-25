ecd94479277ad2ffe09da7ffee99e27d3267c1e9
enricomeloni <enrico-meloni@hotmail.it>
2020-03-25 19:35:13 +0100

    Add draft for charts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Header } from './Views/Header';
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+import { Home } from './Views/Home';
 
 function App() {
-  
-  
+  const hist = createBrowserHistory();
 
 	return (
     <React.Fragment>
       <Header />
-		  <div> Hello world</div>
+      <br />
+      <Router history={hist}>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </React.Fragment> 
 	)
 }
