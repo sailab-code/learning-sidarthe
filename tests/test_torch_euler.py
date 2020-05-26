66ac84e355a8eb01ac052671656184e4a79f4921
Andrea Panizza <8233615+AndreaPi@users.noreply.github.com>
2020-05-26 23:54:51 +0200

    reduce number of digits for expected error constants
    - using less significant digits ensures that test_torch_euler
    passes on different operative systems

--- tests/test_torch_euler.py
+++ tests/test_torch_euler.py
@@ -27,15 +27,15 @@ t_range_fine = integration_interval(steps=1000)
 solution_fine = analytical_solution(t_range_fine)
 
 @pytest.mark.parametrize("method, expected_error, t_range, analytical_solution",
-[(euler, torch.tensor(0.1371068954), t_range_coarse, solution_coarse),
- (Heun, torch.tensor(0.0051426888), t_range_coarse, solution_coarse),
- (RK4, torch.tensor(2.8610229492e-06), t_range_coarse, solution_coarse),
- (euler, torch.tensor(0.0136036873), t_range_medium, solution_medium),
- (Heun, torch.tensor(4.6730041504e-05), t_range_medium, solution_medium),
- (RK4, torch.tensor(2.3841857910e-07), t_range_medium, solution_medium),
- (euler, torch.tensor(0.0013597011), t_range_fine, solution_fine),
- (Heun, torch.tensor(2.3841857910e-06), t_range_fine, solution_fine),
- (RK4, torch.tensor(2.1457672119e-06), t_range_fine, solution_fine),
+[(euler, torch.tensor(0.137107), t_range_coarse, solution_coarse),
+ (Heun, torch.tensor(0.005143), t_range_coarse, solution_coarse),
+ (RK4, torch.tensor(2.861023e-06), t_range_coarse, solution_coarse),
+ (euler, torch.tensor(0.013604), t_range_medium, solution_medium),
+ (Heun, torch.tensor(4.673004e-05), t_range_medium, solution_medium),
+ (RK4, torch.tensor(2.384186e-07), t_range_medium, solution_medium),
+ (euler, torch.tensor(0.001359), t_range_fine, solution_fine),
+ (Heun, torch.tensor(2.384186e-06), t_range_fine, solution_fine),
+ (RK4, torch.tensor(2.145767e-06), t_range_fine, solution_fine),
 ], 
 ids=["euler_coarse", "Heun_coarse", "RK4_coarse",
      "euler_medium", "Heun_medium", "RK4_medium", 
@@ -44,6 +44,6 @@ def test_method(method, expected_error, t_range, analytical_solution):
     numerical_solution = method(f, IC, t_range)
     numerical_solution = numerical_solution.squeeze(1)
     L_inf_err = torch.dist(numerical_solution, analytical_solution, float('inf'))
-    torch.set_printoptions(precision=20)
+    # torch.set_printoptions(precision=20)
     print(L_inf_err)
     assert(torch.isclose(L_inf_err, expected_error,))
