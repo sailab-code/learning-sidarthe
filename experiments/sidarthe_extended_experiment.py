054b9ebcbd1e4a59a076daf30d76fa4a9586ce30
Andrea <a.zugarini91@gmail.com>
2020-09-17 19:55:17 +0200

    initial params for more days and lrs setup

--- experiments/sidarthe_extended_experiment.py
+++ experiments/sidarthe_extended_experiment.py
@@ -29,24 +29,24 @@ class ExtendedSidartheExperiment(SidartheExperiment):
     def make_initial_params(self, **kwargs):
         # default initial_params
         initial_params = {
-            "alpha": [0.570] * 4 + [0.422] * 18 + [0.360] * 6 + [0.210] * 10 + [0.210] * 64,
-            "beta": [0.011] * 4 + [0.0057] * 18 + [0.005] * (17 + 63),
-            "gamma": [0.456] * 4 + [0.285] * 18 + [0.2] * 6 + [0.11] * 10 + [0.11] * 64,
-            "delta": [0.011] * 4 + [0.0057] * 18 + [0.005] * (17 + 63),
-            "epsilon": [0.171] * 12 + [0.143] * 26 + [0.2] * 64,
+            "alpha": [0.570] * 4 + [0.422] * 18 + [0.360] * 6 + [0.210] * 10 + [0.210] * 142,
+            "beta": [0.011] * 4 + [0.0057] * 18 + [0.005] * (17 + 141),
+            "gamma": [0.456] * 4 + [0.285] * 18 + [0.2] * 6 + [0.11] * 10 + [0.11] * 142,
+            "delta": [0.011] * 4 + [0.0057] * 18 + [0.005] * (17 + 141),
+            "epsilon": [0.171] * 12 + [0.143] * 26 + [0.2] * 142,
             "theta": [0.371],
-            "zeta": [0.125] * 22 + [0.034] * 16 + [0.025] * 64,
-            "eta": [0.125] * 22 + [0.034] * 16 + [0.025] * 64,
-            "mu": [0.017] * 22 + [0.008] * (17 + 63),
-            "nu": [0.027] * 22 + [0.015] * (17 + 63),
-            "tau": [0.15] * 102,
-            "lambda": [0.034] * 22 + [0.08] * (17 + 63),
-            "kappa": [0.017] * 22 + [0.017] * 16 + [0.02] * 64,
-            "xi": [0.017] * 22 + [0.017] * 16 + [0.02] * 64,
-            "rho": [0.034] * 22 + [0.017] * 16 + [0.02] * 64,
-            "sigma": [0.017] * 22 + [0.017] * 16 + [0.01] * 64,
-            "phi": [0.02] * 102,
-            "chi": [0.02] * 102
+            "zeta": [0.125] * 22 + [0.034] * 16 + [0.025] * 142,
+            "eta": [0.125] * 22 + [0.034] * 16 + [0.025] * 142,
+            "mu": [0.017] * 22 + [0.008] * (17 + 141),
+            "nu": [0.027] * 22 + [0.015] * (17 + 141),
+            "tau": [0.15],
+            "lambda": [0.034] * 22 + [0.08] * (17 + 141),
+            "kappa": [0.017] * 22 + [0.017] * 16 + [0.02] * 142,
+            "xi": [0.017] * 22 + [0.017] * 16 + [0.02] * 142,
+            "rho": [0.034] * 22 + [0.017] * 16 + [0.02] * 142,
+            "sigma": [0.017] * 22 + [0.017] * 16 + [0.01] * 142,
+            "phi": [0.02] * 180,
+            "chi": [0.02] * 180
         }
         _params = kwargs.get("initial_params", {})
 
@@ -75,26 +75,10 @@ class ExtendedSidartheExperiment(SidartheExperiment):
 
     def make_learning_rates(self, **kwargs):
         # default learning rates
-        learning_rates = {
-            "alpha": 1e-5,
-            "beta": 1e-6,
-            "gamma": 1e-5,
-            "delta": 1e-6,
-            "epsilon": 1e-5,
-            "theta": 1e-7,
-            "xi": 1e-5,
-            "eta": 1e-5,
-            "mu": 1e-5,
-            "nu": 1e-5,
-            "tau": 1e-5,
-            "lambda": 1e-5,
-            "kappa": 1e-5,
-            "zeta": 1e-5,
-            "rho": 1e-5,
-            "sigma": 1e-5,
-            "phi": 1e-5,
-            "chi": 1e-5
-        }
+        learning_rates = super().make_learning_rates(**kwargs)
+        learning_rates["phi"] = 1e-5
+        learning_rates["chi"] = 1e-5
+
         _params = kwargs.get("learning_rates", {})
 
         return self.fill_missing_params(_params, learning_rates)
