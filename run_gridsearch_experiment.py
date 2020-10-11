c30fe052634ae6d9f73d9db92254116547cc39ce
Andrea <a.zugarini91@gmail.com>
2020-10-11 21:17:30 +0200

    grid search FR exp

--- run_gridsearch_experiment.py
+++ run_gridsearch_experiment.py
@@ -9,23 +9,23 @@ from utils.multiprocess_utils import ProcessPool
 import multiprocessing as mp
 import itertools
 
-N_PROCESSES = 6
+N_PROCESSES = 4
 N_PERTURBED_RUNS = 0
 
 if __name__ == "__main__":
     region = "FR"
-    n_epochs = 8000
+    n_epochs = 6000
     t_step = 1.0
     train_size = 182
 
-    der_1st_regs = [5e5, 1e7]
+    der_1st_regs = [1e6, 1e7]
     momentums = [True]
 
-    ms = [0.1]
+    ms = [0.015]
     ass = [0.0]
-    bound_regs = [1.0, 2.5, 5.0]
+    bound_regs = [1e6, 5e5]  # [1.0, 2.5, 5.0]
     loss_type = "nrmse"
-    d_ws, r_ws, t_ws, h_ws, e_ws = [0.5, 1.0], [1.0, 2.0, 3.0], [1.0], [1.0], [1.0]
+    d_ws, r_ws, t_ws, h_ws, e_ws = [3.0], [2.5], [1.5], [1.0], [2.0]
     val_len = 20
 
     # Italy
@@ -52,25 +52,32 @@ if __name__ == "__main__":
 
     # France
     initial_params = {
-        "alpha": [0.25] * train_size,
-        "beta": [0.0055] * train_size,
-        "gamma": [0.2] * train_size,
-        "delta": [0.0057] * train_size,
-        "epsilon": [0.143] * train_size,
-        "theta": [0.371] * train_size,
-        "zeta": [0.0025] * train_size,
-        "eta": [0.0025] * train_size,
+        "alpha": [0.165] * train_size,
+        "beta": [0.005] * train_size,
+        "gamma": [0.11] * train_size,
+        "delta": [0.005] * train_size,
+        "epsilon": [0.1] * train_size,
+        "theta": [0.18] * train_size,
+        "zeta": [0.0034] * train_size,
+        "eta": [0.0034] * train_size,
         "mu": [0.008] * train_size ,
-        "nu": [0.015] * train_size,
-        "tau": [0.02]*train_size,
+        "nu": [0.019] * train_size,
+        "tau": [0.03]*train_size,
         "lambda": [0.07] * train_size,
         "kappa": [0.018] * train_size,
         "xi": [0.018] * train_size,
         "rho": [0.018] * train_size,
-        "sigma": [0.01] * train_size,
+        "sigma": [0.02] * train_size,
         "phi": [0.02] * train_size,
         "chi": [0.02] * train_size
     }
+
+    # initial_params_list = []
+    # for _ in range(10):
+    #     param_gen = SidartheParamGenerator()
+    #     param_gen.random_init(train_size)
+    #     initial_params = param_gen.params
+    #     initial_params_list.append(initial_params)
 
     experiment_cls = ExtendedSidartheExperiment  # switch class to change experiment: e.g. SidartheExperiment
 
@@ -90,15 +97,15 @@ if __name__ == "__main__":
         }
 
         for k, v in loss_weights.items():
-            loss_weights[k] = 0.02 * v # 0.0005
+            loss_weights[k] = 0.0005 * v # 0.0005
 
-        experiment = experiment_cls(region, n_epochs=n_epochs, time_step=t_step, runs_directory=f"runs/FR", uuid_prefix=f"train_size{train_size}_der{der_1st_reg}_m{momentum}_dt_{d_w}")
+        experiment = experiment_cls(region, n_epochs=n_epochs, time_step=t_step, runs_directory=f"runs/FR", uuid_prefix=f"_m{m}_train_size{train_size}_der{der_1st_reg}_m{momentum}_dt_{d_w}_breg{bound_reg}")
         process_pool.start(
             target=experiment.run_exp,
             kwargs={
                 "initial_params": initial_params,
                 "dataset_params": {"train_size": train_size, "val_len": val_len, "region": region},
-                "model_params": {"model_cls": TiedSidartheExtended, "der_1st_reg": der_1st_reg, "bound_reg": bound_reg, "bound_loss_type": "log"},
+                "model_params": {"model_cls": TiedSidartheExtended, "der_1st_reg": der_1st_reg, "bound_reg": bound_reg, "bound_loss_type": "step"},
                 "train_params": {"momentum": momentum, "m": m, "a": a},
                 "loss_weights": loss_weights,
                 "plot_references": False
