1a146ff3f84f6659a68169da72341e44a8f18787
Andrea <a.zugarini91@gmail.com>
2020-11-19 14:29:15 +0100

    WIP adding loggings

--- exp_test.py
+++ exp_test.py
@@ -1,7 +1,20 @@
-from lcm.experiments.sidarthe_experiment import SidartheExperiment
+import os
+from pytorch_lightning.loggers import TensorBoardLogger
 
+from lcm.trainers.sidarthe_trainer import SidartheTrainer
+from lcm.callbacks.json_logging import JsonLoggingCallback
+from lcm.callbacks.tensorboard_logging import TensorboardLoggingCallback
+from lcm.callbacks.print_callback import PrintCallback
 
-exp = SidartheExperiment("ITA", 50, 1., "prova", "")
+exps_path = os.path.join(os.getcwd(), "prova")
+tb_logger = TensorBoardLogger(exps_path, name="tb_logs")
+exp = SidartheTrainer("ITA", 1., "prova", "",
+                      max_epochs=5,
+                      default_root_dir=exps_path,
+                      check_val_every_n_epoch=50,
+                      gradient_clip_val=20.0,
+                      logger=tb_logger,
+                      callbacks=[JsonLoggingCallback(), PrintCallback(), TensorboardLoggingCallback()])
 exp.run_exp(
     dataset_params={
         "train_size": 20
