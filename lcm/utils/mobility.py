386a0b819ed63ca0165d9f5e2cf72a9a9f784c0b
enricomeloni <enrico-meloni@hotmail.it>
2021-01-05 16:51:06 +0100

    Speed up reading of mobility csv
    Fix dtype guessing warning (which slowed down a lot)

--- lcm/utils/mobility.py
+++ lcm/utils/mobility.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import torch
+
 #######
 # GOOGLE
 #######
@@ -19,19 +20,49 @@ def get_google_mobility(locations, first_date):
     :return: a dataframe containing such locations
     """
 
-    locations = [mobility_map[location] if location in mobility_map else location for location in locations]
+    # include only used columns to save memory and time
+    percent_change_columns = [
+        f"{key}_percent_change_from_baseline"
+        for key in [
+            "retail_and_recreation",
+            "grocery_and_pharmacy",
+            "transit_stations",
+            "workplaces"
+        ]
+    ]
 
-    df_google = pd.read_csv(os.path.join(os.getcwd(), 'data', 'Global_Mobility_Report.csv'))
-    df_google['mobility'] = (df_google['retail_and_recreation_percent_change_from_baseline'] + df_google['grocery_and_pharmacy_percent_change_from_baseline'] +df_google['transit_stations_percent_change_from_baseline'] +df_google['workplaces_percent_change_from_baseline'])/4
-    df_google = df_google[((df_google['country_region'].isin(locations)) & (df_google['sub_region_1'].isnull())) | (df_google['sub_region_1'].isin(locations))]
+    used_cols = [
+        *percent_change_columns,
+        "country_region",
+        "sub_region_1",
+        "date"
+    ]
+
+    df_google = pd.read_csv(os.path.join(os.getcwd(), 'data', 'Global_Mobility_Report.csv'), usecols=used_cols)
+
+    # sum percent changes
+    df_google['mobility'] = sum([
+        df_google[key] for key in percent_change_columns]
+    ) / 4
+
+    # filter out locations
+    locations = [mobility_map[location] if location in mobility_map else location for location in locations]
+    country_region_in_locations = df_google['country_region'].isin(locations)
+    is_subregion1_null = df_google['sub_region_1'].isnull()
+    subregion1_in_locations = df_google['sub_region_1'].isin(locations)
+    df_google = df_google[
+        (country_region_in_locations & is_subregion1_null) | subregion1_in_locations
+        ]
+
     df_google = df_google[['sub_region_1', 'country_region', 'mobility', 'date']]
     df_google['sub_region_1'] = df_google['sub_region_1'].fillna(df_google['country_region'])
     df_google = pd.pivot_table(df_google, index=['date'], values='mobility', columns='sub_region_1')
     df_google.index = pd.to_datetime(df_google.index)
     df_google.plot(title='Google')
-    df_google = 1 + df_google/100.
+    df_google = 1 + df_google / 100.
 
     # df_google = df_google['Italy']
+    # filter by first date onward
     df_google = df_google.loc[first_date[0].split("T")[0]:, :]
     df_google = torch.tensor(df_google.values, requires_grad=False, dtype=torch.float32)
-    return df_google # fixme va sistemato per regione
+    return df_google  # fixme va sistemato per regione
