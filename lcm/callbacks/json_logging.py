1deabc19d8e9733469ff29369a6400abec6b6cc3
enricomeloni <enrico-meloni@hotmail.it>
2021-02-02 19:01:55 +0100

    Save final params at end of training

--- lcm/callbacks/json_logging.py
+++ lcm/callbacks/json_logging.py
@@ -4,6 +4,7 @@ import json
 
 from pytorch_lightning import Callback
 
+from lcm.compartmental_model import CompartmentalModel
 from lcm.trainers import CompartmentalTrainer
 
 
@@ -25,3 +26,16 @@ class JsonLoggingCallback(Callback):
         os.makedirs(os.path.dirname(settings_path), exist_ok=True)
         with open(settings_path, "a") as f:
             f.write(json_description)
+
+    def on_train_end(self, trainer, pl_module: CompartmentalModel):
+        final_params = pl_module.params
+
+        np_params = {
+            p_name: p_value.detach().cpu().flatten().tolist()
+            for p_name, p_value in final_params.items()
+        }
+
+        json_file = "final_params.json"
+        settings_path = os.path.join(trainer.exp_path, trainer.logger.name, f"version_{trainer.logger.version}", json_file)
+        with open(settings_path, "a") as f:
+            json.dump(np_params, f, indent=4)
